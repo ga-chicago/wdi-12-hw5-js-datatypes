4f1fad05b20c031a683fba3ca665fa5335f7ff2d
Badal Moradia <badal@i360llc.com>
2018-06-18 18:24:47 -0500

    4th commit - accessed limbo

--- js/app.js
+++ js/app.js
@@ -75,7 +75,28 @@ console.log(crazyObject.larry.nicknames[1]);
 console.log(crazyObject.larry.characters[1]);
 
 
+//Object-ception
+//change the value of limbo to null
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
 
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
 
 
 

f69c05fada03210de63d0ced2663f93a47f86850
Ron Clark <ronaldtclark@gmail.com>
2018-06-18 20:01:48 -0500

    1st commit - selecting datatypes

---
+++ js/app.js
@@ -0,0 +1,62 @@
+
+// ANSWER THE FOLLOWING
+
+// 1. Boolean
+    let lightSwitch;
+      if (lightSwitch === 'on') {
+        console.log(true)
+      } else {
+        console.log(false)
+      };
+
+// 2. String
+    'ronaldtclark@gmail.com'
+
+// 3. Array of strings
+    const spaceShip = ['hull', 'laser blasters', 'tractor beam', 'warp drive'];
+
+// 4. Array of strings
+    const classmates = ['Ron', 'Badal', 'Jay', 'Stephanie', 'Eugene'];
+
+// 5. Object of strings
+    const classmateLocations = {
+      Ron: 'Chicago',
+      Badal: 'New York',
+      Jay: 'Phoenix', 
+      Stephanie: 'Seattle', 
+      Eugene: 'Los Angeles'
+    };
+
+// 6. Object of objects of strings and arrays of strings
+    const classmateInfo = {
+      Ron: { 
+        location: 'Chicago',
+        favTvShows: ['Breaking Bad', 'The Office']
+      },
+      Badal: {
+        location: 'New York',
+        favTvShows: ['How I Met Your Mother', 'Parks & Rec']
+      },
+      Jay: {
+        location: 'Phoenix', 
+        favTvShows: ['Seinfeld', 'Star Trek']
+      },
+      Stephanie: {
+        location: 'Seattle', 
+        favTvShows: ['One Tree Hill', 'Smallville']
+      },
+      Eugene: {
+        location: 'Los Angeles',
+        favTvShows: ['Murder She Wrote', 'Golden Girls']
+      }
+    };
+
+
+
+
+
+
+
+
+
+
